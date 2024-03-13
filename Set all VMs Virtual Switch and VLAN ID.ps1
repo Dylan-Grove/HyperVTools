68e397eec8ba98417f988c0291832d8e0e0a9cbb
Dylan Grove <Dylangrove@outlook.com>
2024-03-13 14:18:32 -0600

    Update Set all VMs Virtual Switch and VLAN ID.ps1

--- Set all VMs Virtual Switch and VLAN ID.ps1
+++ Set all VMs Virtual Switch and VLAN ID.ps1
@@ -1,2 +1,3 @@
 Get-VM | Get-VMNetworkAdapter | Connect-VMNetworkAdapter -SwitchName $Switch
 Get-VM | Set-VMNetworkAdapterVlan -VlanId $ID -Access
+Get-VM | Where-Object {$_.Name -like "*P39*"}| Set-VMNetworkAdapterVlan -VlanId 11 -Access
